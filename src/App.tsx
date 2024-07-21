c08002b749ad0702e35062d73ff9d8d26f437793
Sarkaz <mephis2015@protonmail.com>
2024-07-21 19:06:55 +0700

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import router from "./routes/Route.tsx"
 import { RouterProvider } from "react-router-dom"
+import "@Assets/styles/index.css"
 
 function App() {
     return <RouterProvider router={router} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-    important: true,
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/hooks/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
